c04e034c17d3ec374fe3177973f75664aa0470d6
Shukur Huseynli <bjksukur@gmail.com>
2023-10-25 11:42:37 +0400

    add `Lexend` fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,14 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
     "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],  theme: {
-    extend: {},
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ['Lexend-Regular','sans-serif'],
+        lexendMd: ['Lexend-Medium', 'sans-serif']
+      },
+    },
   },
   darkMode: "class",
   plugins: [nextui()],
