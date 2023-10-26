ec4c6ad372b264921d97c8c7489592edbc3d8ee7
Shukur Huseynli <bjksukur@gmail.com>
2023-10-26 11:57:28 +0400

    update route

--- src/routers/route.tsx
+++ src/routers/route.tsx
@@ -1,8 +1,6 @@
 import App from "../App";
 import Movie from "../pages/Movie";
 import MovieDetail from "../pages/MovieDetail";
-import { MovieDetailsLoader } from "../pages/MovieDetail";
-
 import { createBrowserRouter } from "react-router-dom";
 
 export const router = createBrowserRouter([
@@ -12,14 +10,10 @@ export const router = createBrowserRouter([
   },
   {
     path: "/movie",
-    element: <Movie />,
-    loader: ()=>MovieDetailsLoader,
-    children: [
-      {
-        path:"/movie/:movieId",
-        element: <MovieDetail/>,
-      }
-    ]
+    element: <Movie />
   },
-  
+  {
+    path: "/movie/:id",
+    element: <MovieDetail />,
+  },
 ]);
