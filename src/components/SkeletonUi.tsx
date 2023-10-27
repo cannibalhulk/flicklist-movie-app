4ef372d8819de8a919899f38202ce53ea9b03764
Shukur Huseynli <bjksukur@gmail.com>
2023-10-27 10:16:09 +0400

    update skeleton ui

--- src/components/SkeletonUi.tsx
+++ src/components/SkeletonUi.tsx
@@ -3,15 +3,36 @@ import { Skeleton } from '@nextui-org/react'
 
 function SkeletonUi() {
   return (
-    <div className='flexs'>
+    <div className='flex'>
         <div className="flex flex-col items-center space-y-5 mr-10">
             <Skeleton disableAnimation={false} className='bg-white/30 rounded-lg'>
                 <div className='w-[300px] h-[500px] rounded-lg'></div>
             </Skeleton>
 
-            <Skeleton>
-                <div className='min-w-1/4 h-[40px] '></div>
+            <Skeleton disableAnimation={false} className='bg-white/30 rounded-lg'>
+                <div className='min-w-[100px] h-[40px] '></div>
             </Skeleton>
+        </div>
+
+        <div className=" flex flex-col justify-between space-y-4">
+          <div className="flex flex-col space-y-7">
+            <Skeleton disableAnimation={false} className='bg-white/30 rounded-lg'>
+                <div className='min-w-[400px] h-[30px] '></div>
+            </Skeleton>
+
+            <Skeleton disableAnimation={false} className='bg-white/30 rounded-lg'>
+                <div className='min-w-[200px] h-[80px]'></div>
+            </Skeleton>
+
+            <div className="flex">
+              <Skeleton disableAnimation={false} className='mr-5 bg-white/20 rounded-xl'>
+                  <div className='min-w-[90px] h-[20px] '></div>
+              </Skeleton>
+              <Skeleton disableAnimation={false} className='mr-5 bg-white/20 rounded-xl'>
+                  <div className='min-w-[90px] h-[20px] '></div>
+              </Skeleton>
+            </div>
+          </div>
         </div>
     </div>
   )
