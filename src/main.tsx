4daa56b842ccf5186f2d98e5777f49c830cd3077
Shukur Huseynli <bjksukur@gmail.com>
2023-10-31 11:10:31 +0400

    add recoil, update tasks and eslint config

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { router } from "./routers/route";
 import { RouterProvider } from "react-router-dom";
 import { NextUIProvider } from "@nextui-org/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RecoilRoot } from "recoil";
 
 const queryClient = new QueryClient(); 
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <NextUIProvider>
-      <RouterProvider router={router} />
-    </NextUIProvider>
-  </QueryClientProvider>
+  <RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <NextUIProvider>
+        <RouterProvider router={router} />
+      </NextUIProvider>
+    </QueryClientProvider>
+  </RecoilRoot>
 );
