fb9a7977715e70f7dd86860adaacf012e8de7ea8
Cannibalhulk <82077880+Cannibalhulk@users.noreply.github.com>
2023-11-02 22:54:51 +0400

    add some text-shadow

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -12,8 +12,8 @@ export default function Movie() {
   }
   return (
     <div className="flex min-h-screen pt-3 bg-[#3f3f3f] flex-col items-center  text-xl">
-      <h1 className="py-3 font-lexendMd font-bold text-[35px] mb-5 flex items-end space-x-1">
-        <p>Flicklist</p>
+      <h1 className="py-3 font-lexendMd font-bold text-[35px] mb-5 flex items-end space-x-1 ">
+        <p className="drop-shadow-[0_3px_3px_rgba(225,225,225,1)]">Flicklist</p>
         <div className="rounded-full w-[10px]  h-[10px] bg-[#68a8e9]"></div>
         <div className="rounded-full w-[10px]  h-[10px] bg-[#e9686e]"></div>
         <div className="rounded-full w-[10px]  h-[10px] bg-[#68e9a2]"></div>
