6a0719ed8aa6fa95fd7f80eecdf263191b71847b
Shukur Huseynli <bjksukur@gmail.com>
2023-11-08 15:11:46 +0400

    add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 
 import { Link } from "react-router-dom";
-
+import { Analytics } from '@vercel/analytics/react';
 const App = () => {
   return (
     <div className="bg-[#444] text-red text-3xl min-h-screen">
+      <Analytics/>
       <h1 className="font-thin">
         <Link to={"/movie"}>Movies</Link>
       </h1>
