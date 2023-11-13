4a811c6a2e966497c55310f0eb64212cd0f58bb3
Shukur Huseynli <bjksukur@gmail.com>
2023-11-13 12:10:07 +0400

    change title

--- src/pages/Movie.tsx
+++ src/pages/Movie.tsx
@@ -15,7 +15,7 @@ export default function Movie() {
     <div className="flex min-h-screen pt-3 bg-[#3f3f3f] flex-col items-center  text-xl">
       <h1 className="py-3 font-lexendMd font-bold text-[35px] mb-5 flex items-end space-x-1 ">
         <Link to={'/movie'}>
-          <p className="drop-shadow-[0_3px_3px_rgba(225,225,225,1)]">Flicklist</p>
+          <p className="drop-shadow-[0_3px_3px_rgba(225,225,225,1)]">FlickList</p>
         </Link>
         <div className="rounded-full w-[10px]  h-[10px] bg-[#68a8e9]"></div>
         <div className="rounded-full w-[10px]  h-[10px] bg-[#e9686e]"></div>
